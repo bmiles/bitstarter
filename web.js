dcbd228dfde8501e594522b0cdc43a31ab1a4684
Ben Miles <ben.miles2@gmail.com>
2013-07-09 16:18:17 +0100

    changed webjs to ppull greeting from index.html

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(greeting);
 });
+
+var buf = fs.readFileSync('index.html');
+
+
+var greeting = buf.toString('utf8',0,buf.length);
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
